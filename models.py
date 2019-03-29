31ab9b2e0c77a05efc9b574ca91f71df2ab3070c
Jonathan <jonathandrewkerr@gmail.com>
2019-03-29 19:21:28 +0000

    Clean up characters
    I've tidied up the character actions and added a new class: Party. It'll just contain everyone in the party for now, but we might extend it later.

--- models.py
+++ models.py
@@ -1,13 +1,19 @@
+from typing import List
+
+
 class Character(object):
-    def __init__(self, name):
+    def __init__(self, name='bob'):
         self.first_name = name
 
-    def attack(self):
+    def attack(self, target):
+        pass
+
+    def special(self, target=None):
         pass
 
 
 class Alien(Character):
-    def __init__(self, name, planet):
+    def __init__(self, name='Xzlty', planet='Earth'):
         super().__init__(name)
         self.planet = planet
 
@@ -22,3 +28,8 @@ class Coder(Character):
 
 class Healer(Character):
     pass
+
+
+class Party(object):
+    def __init__(self, members: List[Character]):
+        self.party_members = members
